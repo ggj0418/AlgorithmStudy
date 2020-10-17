32ba37997ec51dcb87e5b64e69dc62f7ecb6900b
ggj0418 <44552228+ggj0418@users.noreply.github.com>
2020-10-17 13:19:08 +0900

    Update 다음 순열.java

--- 브루트 포스/다음 순열.java
+++ 브루트 포스/다음 순열.java
@@ -14,33 +14,34 @@ public class Main {
 		for(int i=0;i<N;i++) array[i] = Integer.parseInt(splitArray[i]);
 		
 		for(int i=N-1;i>=0;i--) {
-		    if(i == 0) {
-		        flag = true;
-		        break;
-		    }
-		    if(array[i] > array[i-1]) {
-                int left = i-1;
-                int right = array.length-1;
-                while(left < right) {
-                    if(array[left] <= array[right]) {
-                        int temp = array[left];
-                        array[left] = array[right];
-                        array[right] = temp;
-                        break;
-                    }
-                    right--;
-                }
+			if(i == 0) {
+				flag = true;
+		        	break;
+			}
+			if(array[i] > array[i-1]) {
+			int left = i-1;
+			int right = array.length-1;
+				
+			while(left < right) {
+				if(array[left] <= array[right]) {
+                        		int temp = array[left];
+                        		array[left] = array[right];
+                        		array[right] = temp;
+                        		break;
+                    		}
+                    		right--;
+			}
                 
-                left = i;
-                right = array.length-1;
+                	left = i;
+                	right = array.length-1;
                 
-                while(left < right) {
-                    int temp2 = array[left];
-		            array[left] = array[right];
-		            array[right] = temp2;
-		            left++;
-		            right--;
-                }
+                	while(left < right) {
+				int temp2 = array[left];
+				array[left] = array[right];
+				array[right] = temp2;
+				left++;
+				right--;
+                	}
                 
 		        break;
 		    }
@@ -48,7 +49,7 @@ public class Main {
 		
 		if(flag) System.out.println(-1);
 		else {
-		    for(int i : array) System.out.print(i + " ");
+			for(int i : array) System.out.print(i + " ");
 		}
 	}
 }
