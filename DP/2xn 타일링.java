e6b3bb476b8a4333662abf5af6e76fcbf98e471c
ggj0418 <44552228+ggj0418@users.noreply.github.com>
2020-10-29 19:03:36 +0900

    Update 2xn 타일링.java

--- DP/2xn 타일링.java
+++ DP/2xn 타일링.java
@@ -3,27 +3,27 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bufR = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bufW = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufR = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufW = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int num = Integer.parseInt(bufR.readLine());
+        int num = Integer.parseInt(bufR.readLine());
         if(num == 1) {
             bufW.write(1 + "\n");
         } else {
             int[] dp = new int[num+1];
-		    dp[1] = 1;
-		    dp[2] = 2;
+            dp[1] = 1;
+            dp[2] = 2;
 		
-		    for(int i=3;i<=num;i++) dp[i] = ( dp[i-1] + dp[i-2] ) % 10007;
+            for(int i=3;i<=num;i++) dp[i] = ( dp[i-1] + dp[i-2] ) % 10007;
 		
-		    bufW.write(dp[num] + "\n");
+            bufW.write(dp[num] + "\n");
         }
 		
-		bufW.flush();
-		bufW.close();
-		bufR.close();
-	}
+        bufW.flush();
+        bufW.close();
+        bufR.close();
+    }
 }
 
 // 쉬운 bottom to up dp 문제이지만 1이라는 케이스를 처리 안해줘서 자꾸 틀렸던 문제이다
