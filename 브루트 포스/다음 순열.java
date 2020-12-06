008aea2d0ac4809776b2d1d64e8bc37e97d6e12e
ggj0418 <44552228+ggj0418@users.noreply.github.com>
2020-12-06 18:29:00 +0900

    Update 다음 순열.java

--- 브루트 포스/다음 순열.java
+++ 브루트 포스/다음 순열.java
@@ -3,53 +3,53 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bufR = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufR = new BufferedReader(new InputStreamReader(System.in));
 		
-		boolean flag = false;
-		int N = Integer.parseInt(bufR.readLine());
-		int[] array = new int[N];
-		String[] splitArray = bufR.readLine().split(" ");
+        boolean flag = false;
+        int N = Integer.parseInt(bufR.readLine());
+        int[] array = new int[N];
+        String[] splitArray = bufR.readLine().split(" ");
 		
-		for(int i=0;i<N;i++) array[i] = Integer.parseInt(splitArray[i]);
+        for(int i=0;i<N;i++) array[i] = Integer.parseInt(splitArray[i]);
 		
-		for(int i=N-1;i>=0;i--) {
-			if(i == 0) {
-				flag = true;
-		        	break;
-			}
-			if(array[i] > array[i-1]) {
-			int left = i-1;
-			int right = array.length-1;
+        for(int i=N-1;i>=0;i--) {
+            if(i == 0) {
+                flag = true;
+                break;
+            }
+            if(array[i] > array[i-1]) {
+                int left = i-1;
+                int right = array.length-1;
 				
-			while(left < right) {
-				if(array[left] <= array[right]) {
-                        		int temp = array[left];
-                        		array[left] = array[right];
-                        		array[right] = temp;
-                        		break;
-                    		}
-                    		right--;
-			}
+                while(left < right) {
+                    if(array[left] <= array[right]) {
+                        int temp = array[left];
+                        array[left] = array[right];
+                        array[right] = temp;
+                        break;
+                    }
+                    right--;
+                }
                 
-                	left = i;
-                	right = array.length-1;
+                left = i;
+                right = array.length-1;
                 
-                	while(left < right) {
-				int temp2 = array[left];
-				array[left] = array[right];
-				array[right] = temp2;
-				left++;
-				right--;
-                	}
+                while(left < right) {
+                    int temp2 = array[left];
+                    array[left] = array[right];
+                    array[right] = temp2;
+                    left++;
+                    right--;
+                }
                 
-		        break;
-		    }
-		}
+                break;
+            }
+        }
 		
-		if(flag) System.out.println(-1);
-		else {
-			for(int i : array) System.out.print(i + " ");
-		}
-	}
+        if(flag) System.out.println(-1);
+        else {
+            for(int i : array) System.out.print(i + " ");
+        }
+    }
 }
