6658f074164936cc4f9375e541b27157813631c3
ggj0418 <44552228+ggj0418@users.noreply.github.com>
2021-02-07 09:36:06 +0900

    Update 한수.java

--- 브루트 포스/한수.java
+++ 브루트 포스/한수.java
@@ -5,25 +5,25 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        	BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-        int N = Integer.parseInt(in.readLine());
+        	int N = Integer.parseInt(in.readLine());
         
-        out.write(go(N) + "\n");
-        out.flush();
-        out.close();
-        in.close();
+        	out.write(go(N) + "\n");
+        	out.flush();
+ 	       out.close();
+        	in.close();
 	}
  
 	private static int go(int N) {
 		int answer = 0;
  
-        // 1자리 혹은 2자리 수의 경우 모든 경우가 등차수열이므로 숫자 그대로 리턴
+        	// 1자리 혹은 2자리 수의 경우 모든 경우가 등차수열이므로 숫자 그대로 리턴
 		if (N < 100) {
 			return N;
 		} 
-        // 3자리의 경우 99까지의 수는 모두 등차수열이므로 미리 더해준다
-        else {
+	        // 3자리의 경우 99까지의 수는 모두 등차수열이므로 미리 더해준다
+	        else {
 			answer = 99;
  
 			for (int i=100;i<=N;i++) {
@@ -31,7 +31,7 @@ public class Main {
 				int ten = (i / 10) % 10;
 				int one = i % 10;
  
-                // 등차수열인 경우 answer++
+        		        // 등차수열인 경우 answer++
 				if ((hundred - ten) == (ten - one)) {
 					answer++;
 				}
