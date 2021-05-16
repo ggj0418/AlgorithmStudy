d0fdc23bd4e2655f312c79051e58ee27e7ec4a3d
ggj0418 <44552228+ggj0418@users.noreply.github.com>
2021-05-17 04:33:15 +0900

    Update 수 찾기.java

--- 이분 탐색/수 찾기.java
+++ 이분 탐색/수 찾기.java
@@ -4,33 +4,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		List<Integer> list = new ArrayList<>();
         
-        int n = Integer.parseInt(in.readLine());
-        StringTokenizer st = new StringTokenizer(in.readLine());
-        while(n-- > 0) {
-            list.add(Integer.parseInt(st.nextToken()));
-        }
-        Collections.sort(list);
+        	int n = Integer.parseInt(in.readLine());
+        	StringTokenizer st = new StringTokenizer(in.readLine());
+        	while(n-- > 0) {
+            		list.add(Integer.parseInt(st.nextToken()));
+        	}
+        	Collections.sort(list);
         
-        int m = Integer.parseInt(in.readLine());
-        st = new StringTokenizer(in.readLine());
+        	int m = Integer.parseInt(in.readLine());
+        	st = new StringTokenizer(in.readLine());
         
-        while(m-- > 0) {
-            int index = Collections.binarySearch(list, Integer.parseInt(st.nextToken()));
-            if(index >= 0) {
-                out.write(1 + "\n");
-            }
-            else {
-                out.write(0 + "\n");
-            }
-        }
+        	while(m-- > 0) {
+            		int index = Collections.binarySearch(list, Integer.parseInt(st.nextToken()));
+            		if(index >= 0) {
+                		out.write(1 + "\n");
+            		}
+            		else {
+                		out.write(0 + "\n");
+            		}
+        	}
         
-        out.flush();
-        out.close();
-        in.close();
+        	out.flush();
+        	out.close();
+        	in.close();
 	}
 }
